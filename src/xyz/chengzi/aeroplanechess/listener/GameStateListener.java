65d0e8586f0241dac6f7e8e9acfcc1f40979a0a1
myoung1032 <myoung1032@gmail.com>
2020-11-23 17:05:00 +0800

    init

---
+++ src/xyz/chengzi/aeroplanechess/listener/GameStateListener.java
@@ -0,0 +1,9 @@
+package xyz.chengzi.aeroplanechess.listener;
+
+public interface GameStateListener extends Listener {
+
+
+    void onPlayerStartRound(int player);
+
+    void onPlayerEndRound(int player);
+}
---
+++ src/xyz/chengzi/aeroplanechess/listener/ab.java
@@ -0,0 +1,13 @@
+package xyz.chengzi.aeroplanechess.listener;
+
+public abstract class ab {
+
+    int age;
+
+     abstract void eat();
+
+
+
+
+
+}
